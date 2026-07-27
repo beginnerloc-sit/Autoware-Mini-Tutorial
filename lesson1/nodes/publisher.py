c8c2cf0db60dff0eccc2b8906b11aff010fd8651
tien loc <loctientran235@gmail.com>
2026-07-27 13:32:26 +0300

    finished lesson 1

---
+++ lesson1/nodes/publisher.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import rospy
+from std_msgs.msg import String
+
+class Publisher:
+    # Parameters
+    def __init__(self):
+        self.message = rospy.get_param('~message', 'Hello World!')
+        self.rate = rospy.get_param('~rate', 2.0)
+
+        # Internal variables
+        self.rate = rospy.Rate(self.rate)
+
+        # Publishers
+        self.pub = rospy.Publisher('/message', String, queue_size=10)
+
+    def run(self):
+        while not rospy.is_shutdown():
+            self.pub.publish(self.message)
+            self.rate.sleep()
+
+if __name__ == '__main__':
+    rospy.init_node('publisher')
+    node = Publisher()
+    node.run()
+
